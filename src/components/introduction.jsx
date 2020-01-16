b35a74c3eb5948e213366744f4cf89a4098c6dca
Christian Miljkovic <cmm892@stern.nyu.edu>
2020-01-16 12:23:36 -0500

    New changes

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(../../public/images/img_bg.JPG)'}}>
+              <li style={{backgroundImage: 'url(images/img_bg.JPG)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -22,7 +22,7 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(../../public/images/img_bg.JPG)'}}>
+              <li style={{backgroundImage: 'url(images/img_bg.JPG)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
